b44eb11714515649e6caab14bb831f23300e30cf
workramsah <workramsah@gmail.com>
2026-02-13 18:40:40 +0545

    add new

--- app/testpod/page.jsx
+++ app/testpod/page.jsx
@@ -15,7 +15,7 @@ const AllProducts = () => {
             <Navbar />
             <div className="flex flex-col items-start px-6 md:px-16 lg:px-32">
                 <div className="flex flex-col items-end pt-12">
-                    <p className="text-2xl font-medium">All products</p>
+                    <p className="text-2xl font-medium">All product</p>
                     <div className="w-16 h-0.5 bg-orange-600 rounded-full"></div>
                 </div>
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-12 pb-14 w-full">
