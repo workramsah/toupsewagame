4542ac7b185c4cb53debd63369a22c549757ef62
workramsah <workramsah@gmail.com>
2026-03-28 22:48:44 +0545

    update price

--- app/page.jsx
+++ app/page.jsx
@@ -17,8 +17,8 @@ const Home = () => {
       <div className="px-6 md:px-16 lg:px-32">
         <HeaderSlider />
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-12 pb-14 w-full">
-          <Link href="/testlistpod"><Testprodcard name="Free Fire" price="Rs 25-1200" description="This is freefire topup" image="freefireimage.jpeg"/></Link>
-          <Link href="/topup/pubg"><Testprodcard name="PUBG" price="Rs 25-1200+" description="This is pubg topup" image="pubgs.avif"/></Link>
+          <Link href="/freefire"><Testprodcard name="Free Fire" price="Rs 99-1000" description="This is freefire topup" image="freefireimage.jpeg"/></Link>
+          <Link href="/topup/pubg"><Testprodcard name="PUBG" price="Rs 169-1200+" description="This is pubg topup" image="pubgs.avif"/></Link>
           
         
         </div>
