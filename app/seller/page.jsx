da8680ea5171b219e175941fa16e6534c653fc70
workramsah <workramsah@gmail.com>
2026-06-05 22:20:07 +0545

    just for test

--- app/seller/page.jsx
+++ app/seller/page.jsx
@@ -21,7 +21,7 @@ const AddProduct = () => {
     <div className="flex-1 min-h-screen flex flex-col justify-between">
       <form onSubmit={handleSubmit} className="md:p-10 p-4 space-y-5 max-w-lg">
         <div>
-          <p className="text-base font-medium">Product Image</p>
+          <p className="text-base font-medium">Product Images</p>
           <div className="flex flex-wrap items-center gap-3 mt-2">
 
             {[...Array(4)].map((_, index) => (
